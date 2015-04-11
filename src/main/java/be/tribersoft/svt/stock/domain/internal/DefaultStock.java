f9f7dcd1a557495faa72b13632440ac890835370
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2015-04-11 13:09:29 +0200

    added equals and to string on stock

--- src/main/java/be/tribersoft/svt/stock/domain/internal/DefaultStock.java
+++ src/main/java/be/tribersoft/svt/stock/domain/internal/DefaultStock.java
@@ -46,4 +46,34 @@ public class DefaultStock implements Stock {
 		this.lowestPrice = lowestPrice;
 	}
 
+	public String toString() {
+		return "Stock: '" + name + "' latest price: " + (latestPrice != null ? latestPrice : "UNKNOWN") + " highest price: " + (highestPrice != null ? highestPrice : "UNKNOWN") + " lowest price "
+				+ (lowestPrice != null ? lowestPrice : "UNKNOWN") + ".";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DefaultStock other = (DefaultStock) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 }
